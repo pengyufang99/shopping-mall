abbe0607bbc17b2ade81f1bb9a4cc10ccfe1b75b
Mora <525524802@qq.com>
2020-04-05 15:59:53 +0800

    完成用户列表功能开发

--- src/main.js
+++ src/main.js
@@ -10,6 +10,12 @@ import '../src/assets/fonts/iconfont.css'
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 请求拦截器
+axios.interceptors.request.use(config => {
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
